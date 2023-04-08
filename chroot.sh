af84275fef4e13260d1de57b7d3114eebc2432a0
Poverty <2971929+poorjobless@users.noreply.github.com>
2023-04-08 09:51:19 +0800

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -1,5 +1,50 @@
 #!/bin/bash
 
+##################################################################################################
+## tinycorelinux
+#TINYCORELINUX_VERSION="13.x"
+#TINYCORELINUX_ARCH="x86"
+#TINYCORELINUX_ROOTFS_NAME="rootfs"
+
+#if [ $TINYCORELINUX_ARCH=="x86_64" ];then
+#  TINYCORELINUX_ROOTFS_NAME="rootfs64"
+#else
+#  TINYCORELINUX_ROOTFS_NAME="rootfs"
+#fi
+
+#echo "----------------------------------------------------"
+#echo -e "Downloading root filesystem\n"
+#wget http://www.tinycorelinux.net/$TINYCORELINUX_VERSION/$TINYCORELINUX_ARCH/release/distribution_files/$TINYCORELINUX_ROOTFS_NAME.gz
+
+##################################################################################################
+## ubuntu base
+#UBUNTUBASE_VERSION="22.10"
+#UBUNTUBASE_ARCH="amd64"
+
+#echo "----------------------------------------------------"
+#echo -e "Downloading root filesystem\n"
+#wget http://cdimage.ubuntu.com/ubuntu-base/releases/$UBUNTUBASE_VERSION/release/ubuntu-base-$UBUNTUBASE_VERSION-base-$UBUNTUBASE_ARCH.tar.gz
+
+##################################################################################################
+## voidlinux
+#VOIDLINUX_VERSION="20221001"
+#VOIDLINUX_ARCH="x86_64"
+#VOIDLINUX_CLIB="glibc"
+#VOIDLINUX_CLIBURL=""
+
+#if [ $VOIDLINUX_CLIB="musl" ];then
+#  VOIDLINUX_CLIBURL="-musl"
+#else
+#  VOIDLINUX_CLIBURL=""
+#fi
+
+#echo "----------------------------------------------------"
+#echo -e "Downloading root filesystem\n"
+#wget https://repo-default.voidlinux.org/live/$VOIDLINUX_VERSION/void-$VOIDLINUX_ARCH$VOIDLINUX_CLIBURL-ROOTFS-$VOIDLINUX_VERSION.tar.xz
+
+
+##################################################################################################
+## alpinelinux
 ALPINE_VERSION="v3.17"
 ALPINE_SUBVERSION="3.17.3"
 ALPINE_ARCH="x86_64"
